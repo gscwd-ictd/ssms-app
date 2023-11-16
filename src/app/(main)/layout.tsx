eea510b8913ec5aaa4d669db250522ed67d88f99
Eric Sison <ericsison.dev@gmail.com>
2023-11-16 08:56:55 +0800

    added page routes

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,12 +1,16 @@
-import { Sidebar } from "@ssms/components/navigations/SideNavigation/Sidebar";
+import { Sidebar } from "@ssms/components/features/navigations/SideNavigation/Sidebar";
+import { PageContainer } from "@ssms/components/features/navigations/ContentNavigation/PageContainer";
 import { LayoutProps } from "../layout";
-import PageContent from "@ssms/components/navigations/PageContent/PageContent";
+import { Topbar } from "@ssms/components/features/navigations/TopNavigation/Topbar";
 
 export default function MainLayout({ children }: LayoutProps) {
   return (
-    <div className="flex h-screen w-screen">
-      <Sidebar />
-      <PageContent>{children}</PageContent>
+    <div className="h-screen w-screen">
+      <Topbar />
+      <div className="flex h-[calc(100%-5rem)] w-full">
+        <Sidebar />
+        <PageContainer>{children}</PageContainer>
+      </div>
     </div>
   );
 }
--- src/app/(main)/requests/page.tsx
+++ src/app/(main)/requests/page.tsx
@@ -1,3 +1,36 @@
-export default function Requests() {
-  return <>Requests</>;
+import { PageTitle } from "@ssms/components/features/navigations/ContentNavigation/PageTitle";
+import { ServiceRequest } from "@ssms/utils/types/serviceRequest";
+import axios from "axios";
+import Image from "next/image";
+
+const getAllRequests = async () => {
+  try {
+    return (await axios.get<ServiceRequest[]>("http://localhost:4000/service-requests")).data;
+  } catch (error) {
+    console.log(error, "this is error");
+  }
+};
+
+export default async function Requests() {
+  const serviceRequests = await getAllRequests();
+
+  return (
+    <>
+      <PageTitle></PageTitle>
+      <ul className="space-y-2">
+        {serviceRequests?.map((request, index) => (
+          <li key={index} className="flex items-center gap-2">
+            <Image
+              src={request.requestor.avatar}
+              alt="profile"
+              width={250}
+              height={250}
+              className="h-10 w-10 rounded-full object-cover"
+            />
+            {request.requestor.fullName}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
